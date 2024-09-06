0ef679495fb277221eef7c6cdc7af5d966c0ad05
kutschl <lukas.kutsch@gmail.com>
2024-09-06 12:21:35 +0000

    launch for race stack and dockerfile and shells

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,3 +4,5 @@ sudo rosdep update
 sudo rosdep install --from-paths /sim_ws/src --ignore-src -y
 sudo chown -R $(whoami) /sim_ws
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --cmake-clean-cache
+chmod +x ./planner_gym.sh
+chmod +x ./race_gym.sh
